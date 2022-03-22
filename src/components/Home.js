f36cf90fb36c38ef6922233bb73e5cb9f830530a
Coding Shinobi <Codingshinobi111@gmail.com>
2022-03-23 02:07:45 +0530

    new commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,74 +1,67 @@
-import React from 'react'
-import styled from 'styled-components'
-import Section from './Section'
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
 function Home() {
   return (
     <Container>
-    <Section 
+      <Section
         title="Model S"
-        description="Order online for touchless delivery"
-        backgroundImg="model-s.jpg"
-        leftBtnText="Custom Order"
-        rightBtnText="Exisiting Inventory"
-
-    />
-    <Section 
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-s.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+        showDownArrow
+      />
+      <Section
         title="Model Y"
-        description="Order online for touchless delivery"
-        backgroundImg="model-y.jpg"
-        leftBtnText="Custom Order"
-        rightBtnText="Exisiting Inventory"
-
-    />
-    <Section 
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-y.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
         title="Model 3"
-        description="Order online for touchless delivery"
-        backgroundImg="model-3.jpg"
-        leftBtnText="Custom Order"
-        rightBtnText="Exisiting Inventory"
-
-    />
-    <Section 
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-3.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
         title="Model X"
-        description="Order online for touchless delivery"
-        backgroundImg="model-x.jpg"
-        leftBtnText="Custom Order"
-        rightBtnText="Exisiting Inventory"
-
-    />
-    <Section 
-        title="Lowest Cost Solar Panels in America"
-        description="Money-back Guarantee "
-        backgroundImg="solar-panel.jpg"
-        leftBtnText="Order now"
-        rightBtnText="Learn More"
-
-    />
-    <Section 
-        title="Solar for New Roofs"
-        description="Solar Roof Costs less than a New Roof plus Solar panels "
-        backgroundImg="solar-roof.jpg"
-        leftBtnText="Order now"
-        rightBtnText="Learn More"
-
-    />
-    <Section 
+        description="Order Online for Touchless Delivery"
+        backgroundImage="model-x.jpg"
+        leftButtonText="Custom Order"
+        rightButtonText="Existing Inventory"
+      />
+      <Section
+        title="Solar Panels"
+        description="Lowest Cost Solar Panels in America"
+        backgroundImage="solar-panel.jpg"
+        leftButtonText="Order Now"
+        rightButtonText="Learn More"
+      />
+      <Section
+        title="Solar Roof"
+        description="Produce Clean Energy From Your Roof"
+        backgroundImage="solar-roof.jpg"
+        leftButtonText="Order Now"
+        rightButtonText="Learn More"
+      />
+      <Section
         title="Accessories"
-        description=" "
-        backgroundImg="accessories.jpg"
-        leftBtnText="Order now"
-        
-
-    />
- 
-    </Container >
-  )
+        description=""
+        backgroundImage="accessories.jpg"
+        leftButtonText="Shop Now"
+      />
+    </Container>
+  );
 }
 
-export default Home
-
+export default Home;
 const Container = styled.div`
-height: 100vh;
-
-
-`
+  height: 100vh;
+  scroll-snap-type: y mandatory;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
